3ac439b696447b16d52f7557be4d52f8bc962624
ayan910 <ayanmajumder06@gmail.com>
2020-09-26 02:37:24 +0530

    UserRegister modify

--- OnlineExam/src/app/app.module.ts
+++ OnlineExam/src/app/app.module.ts
@@ -5,20 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-<<<<<<< HEAD
 
-=======
-import { SetquestionComponent } from './setquestion/setquestion.component';
-import { ListComponent } from './list/list.component';
-import { SearchComponent } from './search/search.component';
-import { UserComponent } from './user/user.component';
-import { UserregisterComponent } from './userregister/userregister.component';
-import { UserloginComponent } from './userlogin/userlogin.component';
-import { UserhomeComponent } from './userhome/userhome.component';
-import { ExamComponent } from './exam/exam.component';
-import { UserreportComponent } from './userreport/userreport.component';
-import { FormsModule } from '@angular/forms';
->>>>>>> ca4d3873f7fb3796332a931a3c97c5e95ac62268
 
 @NgModule({
   declarations: [
@@ -28,13 +15,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
     FormsModule,
     NgbModule.forRoot()
-=======
-    NgbModule.forRoot(),
-    FormsModule
->>>>>>> ca4d3873f7fb3796332a931a3c97c5e95ac62268
   ],
   providers: [],
   bootstrap: [AppComponent]
