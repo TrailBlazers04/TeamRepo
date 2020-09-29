91a68c2e323ef7683811323867e7a0d4d5933768
TrailBlazers04 <sushil.goud@lntinfotech.com>
2020-09-29 16:15:31 +0530

    Test classes for data tables 9 29 16:15 by sushil

--- onlineExamBack/src/main/java/com/lti/entity/JavaQ.java
+++ onlineExamBack/src/main/java/com/lti/entity/JavaQ.java
@@ -18,10 +18,10 @@ public class JavaQ {
 	private int qno;
 	@Column(length = 30)
 	private String question;
-	private int o1;
-	private int o2;
-	private int o3;
-	private int o4;
+	private String o1;
+	private String o2;
+	private String o3;
+	private String o4;
 	private int co;
 	public int getQno() {
 		return qno;
@@ -35,28 +35,28 @@ public class JavaQ {
 	public void setQuestion(String question) {
 		this.question = question;
 	}
-	public int getO1() {
+	public String getO1() {
 		return o1;
 	}
-	public void setO1(int o1) {
+	public void setO1(String o1) {
 		this.o1 = o1;
 	}
-	public int getO2() {
+	public String getO2() {
 		return o2;
 	}
-	public void setO2(int o2) {
+	public void setO2(String o2) {
 		this.o2 = o2;
 	}
-	public int getO3() {
+	public String getO3() {
 		return o3;
 	}
-	public void setO3(int o3) {
+	public void setO3(String o3) {
 		this.o3 = o3;
 	}
-	public int getO4() {
+	public String getO4() {
 		return o4;
 	}
-	public void setO4(int o4) {
+	public void setO4(String o4) {
 		this.o4 = o4;
 	}
 	public int getCo() {
